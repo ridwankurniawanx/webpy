761b7405b0cd58d32549b5df0170c05478747bb4
ridwankurniawanx <65314308+ridwankurniawanx@users.noreply.github.com>
2022-01-07 14:02:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,68 @@
-from pywebio.input import *
+from pywebio import *
 from pywebio.output import *
+from pywebio.pin import *
 
+img_link = "https://www.influxdata.com/wp-content/uploads/histogram.png"
+md = '*Welcome, here is an instruction.....*'
 
+def gdp_chart(plot_type):
+    import plotly.express as px
+
+    df = px.data.gapminder()
+    fig = px.line_geo(df.query('year == 2007'), locations='iso_alpha', 
+        color='continent', projection='orthographic', template="plotly_dark", )
+    fig.update_layout(showlegend=False)
+    
+    animation = px.scatter(df, x='gdpPercap', y='lifeExp', color='continent', size='pop', size_max=40, 
+                hover_name='country', template="plotly_dark", log_x=True, animation_frame='year',
+                 animation_group='country', range_x=[100, 50000], range_y=[20,90])
+                 
+    if plot_type == 'animation':
+        #put_html(gdp_chart('animation')) #put the plot on a pywebio app
+        return animation.to_html(include_plotlyjs="require", full_html=True)
+    elif plot_type == 'geo':
+        #put_html(gdp_chart('geo')) #put the plot on a pywebio app
+        return fig.to_html(include_plotlyjs="require", full_html=True)
+
+@config(theme='dark')
 def main():
-    """BMI Calculation
+    session.set_env(title='My Chart!!', output_max_width='90%')
+    put_grid([
+        [span(put_markdown('## Section A'), col=3)],
+        [put_markdown('### Chart 1'), put_markdown('### Chart 2'), put_markdown('### Chart 3')],
+        [put_scope('haha'), put_scope('1-2'), put_scope('1-3')],
+        [span(put_markdown('## Section B'), col=2, row=1), put_markdown('## Section C')],
+        [put_scope('haha1'), put_scope('haha2'), put_scope('haha3')]
+        #[span(put_row([
+        #        put_select('x', help_text='X column', options=['a', 'b']),
+        #        put_select('y', help_text='Y column', options=['x', 'y']),
+        #        ]), col=2, row=1),
+        #    None, 
+        #],
+        #[span(put_image(img_link), col=2, row=1), put_scope('2-3')],
+    ], cell_widths='33% 33% 33%')
 
-    Simple application for calculating Body Mass Index.
-    """
-
-    put_markdown("""# Body Mass Index
+    with use_scope('1-2'):
+        put_html(gdp_chart('animation'))
     
-    [Body mass index](https://en.wikipedia.org/wiki/Body_mass_index) (BMI) is a measure of body fat based on height and weight that applies to adult men and women. 
+    with use_scope('haha'):
+        put_html(gdp_chart('geo'))
+    with use_scope('haha1'):
+        put_html(gdp_chart('geo'))
+    with use_scope('haha2'):
+        put_html(gdp_chart('geo'))
+    with use_scope('haha3'):
+        put_html(gdp_chart('animation'))
+        
+    with use_scope('1-3'):
+        put_html(gdp_chart('animation'))
+        
+    #with use_scope('2-3'):
+    #    put_markdown(md)
+    #    put_input('something', label='input something to show as a toast message')
+    #    put_button('submit', onclick=lambda: toast(pin.something))
+
+
+
+
     
-    BMI Categories:
-    
-    | Category             | BMI           |
-    | -------------------- | ------------- |
-    | Severely underweight | BMI<14.9      |
-    | Underweight          | 14.9≤BMI<18.4 |
-    | Normal               | 18.4≤BMI<22.9 |
-    | Overweight           | 22.9≤BMI<27.5 |
-    | Moderately obese     | 27.5≤BMI<40   |
-    | Severely obese       | BMI≥40        |
-    
-    ## BMI calculation
-    """, strip_indent=4)
-
-    info = input_group('BMI calculation', [
-        input("Your Height(cm)", name="height", type=FLOAT),
-        input("Your Weight(kg)", name="weight", type=FLOAT),
-    ])
-
-    BMI = info['weight'] / (info['height'] / 100) ** 2
-
-    top_status = [(14.9, 'Severely underweight'), (18.4, 'Underweight'),
-                  (22.9, 'Normal'), (27.5, 'Overweight'),
-                  (40.0, 'Moderately obese'), (float('inf'), 'Severely obese')]
-
-    for top, status in top_status:
-        if BMI <= top:
-            put_markdown('Your BMI: `%.1f`, Category: `%s`' % (BMI, status))
-            break
-
-
-if __name__ == '__main__':
-    import argparse
-    from pywebio.platform.tornado_http import start_server as start_http_server
-    from pywebio import start_server as start_ws_server
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", type=int, default=8080)
-    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
-    args = parser.parse_args()
-
-    if args.http:
-        start_http_server(main, port=args.port)
-    else:
-        # Since some cloud server may close idle connections (such as heroku),
-        # use `websocket_ping_interval` to  keep the connection alive
-        start_ws_server(main, port=args.port, websocket_ping_interval=30)
-
